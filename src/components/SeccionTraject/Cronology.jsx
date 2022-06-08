bcbf4122fde6efb5a29de240158f85df47a3dcaf
Comecocos10 <ahp93524@gmail.com>
2022-06-08 20:09:29 +0200

    Creacion Cronology/ Comprobar que se sube

--- src/components/SeccionTraject/Cronology.jsx
+++ src/components/SeccionTraject/Cronology.jsx
@@ -5,6 +5,18 @@ export default function SeccionTraject(props) {
 
 
     return <div className='crono'>
+        <div className='years'>
+            <p className='year'>2013</p>
+            <p className='year'>2015</p>
+            <p className='year'>2016</p>
+            <p className='year'>2017</p>
+            <p className='year'>2018</p>
+            <p className='year'>2020</p>
+            <p className='year'>2021</p>
+        </div>
+        <div className='arrow-guide'>
+            <img src="" alt="" />
+        </div>
         
     </div>
 }
